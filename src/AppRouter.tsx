ce7d2c97fbf34eb3aafb1230db5472a8ce621aa7
sandro-dev26 <sadrokakhidze@gmail.com>
2026-07-24 16:56:20 +0400

    Add MainLayout to AppRouter

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,10 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router";
+import MainLayout from "./layouts/MainLayout";
+import Home from "./pages/Home";
 
 function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" />
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Home />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
