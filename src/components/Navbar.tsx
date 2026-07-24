7b65599533639d6a3fc43bc837232d6f91b89cfc
sandro-dev26 <sadrokakhidze@gmail.com>
2026-07-24 17:24:05 +0400

    Update Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,26 @@
-import { Link } from "react-router";
+import { NavLink } from "react-router";
 
 function Navbar() {
+  const pages = ["Home", "Aircrafts", "Airlines"];
   return (
-    <nav>
-      <Link to="/">Home</Link>
+    <nav className="flex items-center justify-center">
+      <div className="flex items-center justify-around gap-4 p-4 mt-2 rounded-full bg-sky-400 text-neutral-50">
+        {pages.map((item) => (
+          <NavLink
+            key={item}
+            className={({ isActive }) =>
+              `text-sm transition-colors ${
+                isActive
+                  ? "font-semibold border-b-2 border-neutral-50 transition-all duration-200"
+                  : "font-light border-b-0 border-neutral-50 transition-all duration-200"
+              }`
+            }
+            to={item === "Home" ? "/" : `/${item.toLowerCase()}`}
+          >
+            {item}
+          </NavLink>
+        ))}
+      </div>
     </nav>
   );
 }
