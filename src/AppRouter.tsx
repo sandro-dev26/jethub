397cfcc4852775e5bf08be7915b43c17bd64c17b
sandro-dev26 <sadrokakhidze@gmail.com>
2026-07-24 16:35:55 +0400

    Create base AppRouter structure

---
+++ src/AppRouter.tsx
@@ -0,0 +1,13 @@
+import { BrowserRouter, Routes, Route } from "react-router";
+
+function AppRouter() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default AppRouter;
