5269edcd93034a7557f8dc29913d5e550021cab4
sandro-dev26 <sadrokakhidze@gmail.com>
2026-07-28 03:10:39 +0400

    Fix favorites localstorage bug

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,51 @@
+import { motion } from "motion/react";
 import { fetchAircraftData } from "../fetch";
 import { useState, useEffect } from "react";
 import { useNavigate, Link } from "react-router";
 import type { Aircraft } from "../types/aircraft";
+import { Star } from "lucide-react";
+import airlineData from "../data/airline.json";
+import airportData from "../data/airport.json";
 
 function Home() {
+  const [favAircraft, setFavAircraft] = useState<string[]>(() => {
+    const saved = localStorage.getItem("favAircraft");
+    return saved ? JSON.parse(saved) : [];
+  });
+
+  const [favAirlines, setFavAirlines] = useState<string[]>(() => {
+    const saved = localStorage.getItem("favAirlines");
+    return saved ? JSON.parse(saved) : [];
+  });
+
+  const [favAirports, setFavAirports] = useState<string[]>(() => {
+    const saved = localStorage.getItem("favAirports");
+    return saved ? JSON.parse(saved) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("favAircraft", JSON.stringify(favAircraft));
+  }, [favAircraft]);
+
+  useEffect(() => {
+    localStorage.setItem("favAirlines", JSON.stringify(favAirlines));
+  }, [favAirlines]);
+
+  useEffect(() => {
+    localStorage.setItem("favAirports", JSON.stringify(favAirports));
+  }, [favAirports]);
+
+  const changeFavs = (
+    id: string,
+    setFavs: React.Dispatch<React.SetStateAction<string[]>>,
+  ) => {
+    setFavs((prev) =>
+      prev.includes(id) ? prev.filter((item) => item !== id) : [...prev, id],
+    );
+  };
+
   const [aircraftData, setAircraftData] = useState<Aircraft[] | null>(null);
+
   useEffect(() => {
     async function setData() {
       try {
@@ -43,46 +84,79 @@ function Home() {
           </Link>
         </p>
       </header>
+
       <div className="mt-4">
-        <label htmlFor="aircrafts" className="text-2xl">
-          Aircrafts
+        <label
+          htmlFor="aircrafts"
+          className="text-2xl hover:underline underline-offset-4"
+          onClick={() => {
+            navigate("/aircraft");
+          }}
+        >
+          Aircraft
         </label>
         <section
           id="aircrafts"
           className="flex flex-row flex-2 gap-2 min-h-60 overflow-auto p-4 scrollbar-thumb-sky-500"
         >
-          {aircraftData?.slice(0, 5).map((item) => (
-            <div
-              key={item.id}
-              className="bg-sky-100 dark:bg-sky-900 text-sky-500 w-80 shrink-0 border border-sky-500 p-2 rounded-2xl transition-all duration-200 hover:-translate-y-1 hover:shadow-lg shadow-black/50 dark:shadow-white/50"
-            >
-              <h2
-                onClick={() => {
-                  navigate(`/aircraft/${item.id}`);
-                }}
-                className="text-2xl text-sky-50 [text-shadow:0_1px_2px_rgb(0_0_0/0.5)] font-semibold"
+          {aircraftData?.slice(0, 5).map((aircraft) => {
+            const isFav = favAircraft.includes(aircraft.id);
+
+            return (
+              <div
+                key={aircraft.id}
+                className="bg-sky-100 dark:bg-sky-900 text-sky-500 w-80 shrink-0 border border-sky-500 p-2 rounded-2xl transition-all duration-200 hover:-translate-y-1 hover:shadow-lg shadow-black/50 dark:shadow-white/50"
               >
-                {item.name}
-              </h2>
-              <p>
-                Manufacturer:{" "}
-                <span
+                <h2
                   onClick={() => {
-                    navigate(
-                      `/airlines/${item.manufacturer.toLocaleLowerCase()}`,
-                    );
+                    navigate(`/aircraft/${aircraft.id}`);
                   }}
-                  className="font-light hover:font-normal hover:underline underline-offset-4 transition-all duration-200"
+                  className="text-2xl text-sky-50 [text-shadow:0_1px_2px_rgb(0_0_0/0.5)] font-semibold"
                 >
-                  {item.manufacturer}
-                </span>
-              </p>
-              <p>
-                Category: <span>{item.category}</span>
-              </p>
-              <p>{item.short_description}</p>
-            </div>
-          ))}
+                  {aircraft.name}
+                </h2>
+                <p>
+                  Manufacturer:{" "}
+                  <span
+                    onClick={() => {
+                      navigate(
+                        `/aircraft/${aircraft.manufacturer.toLocaleLowerCase()}`,
+                      );
+                    }}
+                    className="font-light hover:font-normal hover:underline underline-offset-4 transition-all duration-200"
+                  >
+                    {aircraft.manufacturer}
+                  </span>
+                </p>
+                <p>
+                  Category: <span>{aircraft.category}</span>
+                </p>
+                <p>{aircraft.short_description}</p>
+
+                <motion.button
+                  whileTap={{
+                    scale: 0.8,
+                  }}
+                  whileHover={{
+                    scale: 1.2,
+                    transition: { type: "spring", stiffness: 200, damping: 7 },
+                  }}
+                  className={`self-end p-2 ${isFav ? "text-amber-400" : "text-neutral-400 hover:text-amber-400"}`}
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    changeFavs(aircraft.id, setFavAircraft);
+                  }}
+                >
+                  <Star
+                    size={20}
+                    className={`transition-all duration-300 active:brightness-80
+                    ${isFav ? " fill-amber-400" : "fill-amber-400/0"}
+                  `}
+                  />
+                </motion.button>
+              </div>
+            );
+          })}
           <div
             key="aircrafts-more"
             onClick={() => {
@@ -94,6 +168,158 @@ function Home() {
               MORE
             </h2>
             <p className="text-sm text-sky-50">See more aircrafts</p>
+          </div>
+        </section>
+
+        <label
+          htmlFor="airlines"
+          className="text-2xl hover:underline underline-offset-4"
+          onClick={() => {
+            navigate("/airlines");
+          }}
+        >
+          Airlines
+        </label>
+        <section
+          id="airlines"
+          className="flex flex-row flex-2 gap-2 min-h-60 overflow-auto p-4 scrollbar-thumb-sky-500"
+        >
+          {airlineData?.slice(0, 5).map((airline) => {
+            const isFav = favAirlines.includes(airline.id);
+
+            return (
+              <div
+                key={airline.id}
+                className="bg-sky-100 dark:bg-sky-900 text-sky-500 w-80 shrink-0 border border-sky-500 p-2 rounded-2xl transition-all duration-200 hover:-translate-y-1 hover:shadow-lg shadow-black/50 dark:shadow-white/50"
+              >
+                <h2
+                  onClick={() => {
+                    navigate(`/airlines/${airline.id}`);
+                  }}
+                  className="text-2xl text-sky-50 [text-shadow:0_1px_2px_rgb(0_0_0/0.5)] font-semibold"
+                >
+                  {airline.name}
+                </h2>
+                <p>Callsign: {airline.callsign}</p>
+                <p>
+                  Iata: <span>{airline.iata}</span>
+                </p>
+                <p>
+                  Icao: <span>{airline.icao}</span>
+                </p>
+                <p>{airline.short_description}</p>
+
+                <motion.button
+                  whileTap={{
+                    scale: 0.8,
+                  }}
+                  whileHover={{
+                    scale: 1.2,
+                    transition: { type: "spring", stiffness: 200, damping: 7 },
+                  }}
+                  className={`self-end p-2 ${isFav ? "text-amber-400" : "text-neutral-400 hover:text-amber-400"}`}
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    changeFavs(airline.id, setFavAirlines);
+                  }}
+                >
+                  <Star
+                    size={20}
+                    className={`transition-all duration-300 active:brightness-80
+                    ${isFav ? " fill-amber-400" : "fill-amber-400/0"}
+                  `}
+                  />
+                </motion.button>
+              </div>
+            );
+          })}
+          <div
+            key="aircrafts-more"
+            onClick={() => {
+              navigate("/airlines");
+            }}
+            className="flex flex-col items-center justify-center bg-sky-100 dark:bg-sky-900 text-sky-500 w-80 shrink-0 border border-sky-500 p-2 rounded-2xl transition-all duration-200 hover:-translate-y-1 hover:shadow-lg shadow-black/50 dark:shadow-white/50"
+          >
+            <h2 className="text-4xl text-sky-50 hover:underline underline-offset-2 decoration-1">
+              MORE
+            </h2>
+            <p className="text-sm text-sky-50">See more airlines</p>
+          </div>
+        </section>
+
+        <label
+          htmlFor="airports"
+          className="text-2xl hover:underline underline-offset-4"
+          onClick={() => {
+            navigate("/airports");
+          }}
+        >
+          Airports
+        </label>
+        <section
+          id="airlines"
+          className="flex flex-row flex-2 gap-2 min-h-60 overflow-auto p-4 scrollbar-thumb-sky-500"
+        >
+          {airportData?.slice(0, 5).map((airport) => {
+            const isFav = favAirports.includes(airport.id);
+
+            return (
+              <div
+                key={airport.id}
+                className="bg-sky-100 dark:bg-sky-900 text-sky-500 w-80 shrink-0 border border-sky-500 p-2 rounded-2xl transition-all duration-200 hover:-translate-y-1 hover:shadow-lg shadow-black/50 dark:shadow-white/50"
+              >
+                <h2
+                  onClick={() => {
+                    navigate(`/airports/${airport.id}`);
+                  }}
+                  className="text-2xl text-sky-50 [text-shadow:0_1px_2px_rgb(0_0_0/0.5)] font-semibold"
+                >
+                  {airport.name}
+                </h2>
+                <p>Countury: {airport.country}</p>
+                <p>
+                  Iata: <span>{airport.iata}</span>
+                </p>
+                <p>
+                  Icao: <span>{airport.icao}</span>
+                </p>
+                <p>{airport.shortDescription}</p>
+
+                <motion.button
+                  whileTap={{
+                    scale: 0.8,
+                  }}
+                  whileHover={{
+                    scale: 1.2,
+                    transition: { type: "spring", stiffness: 200, damping: 7 },
+                  }}
+                  className={`self-end p-2 ${isFav ? "text-amber-400" : "text-neutral-400 hover:text-amber-400"}`}
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    changeFavs(airport.id, setFavAirports);
+                  }}
+                >
+                  <Star
+                    size={20}
+                    className={`transition-all duration-300 active:brightness-80
+                    ${isFav ? " fill-amber-400" : "fill-amber-400/0"}
+                  `}
+                  />
+                </motion.button>
+              </div>
+            );
+          })}
+          <div
+            key="aircrafts-more"
+            onClick={() => {
+              navigate("/airports");
+            }}
+            className="flex flex-col items-center justify-center bg-sky-100 dark:bg-sky-900 text-sky-500 w-80 shrink-0 border border-sky-500 p-2 rounded-2xl transition-all duration-200 hover:-translate-y-1 hover:shadow-lg shadow-black/50 dark:shadow-white/50"
+          >
+            <h2 className="text-4xl text-sky-50 hover:underline underline-offset-2 decoration-1">
+              MORE
+            </h2>
+            <p className="text-sm text-sky-50">See more airlines</p>
           </div>
         </section>
       </div>
