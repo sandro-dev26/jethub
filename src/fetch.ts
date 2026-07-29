8fc7424958c69875e3b1ff2c390df21616a0d7c9
sandro-dev26 <sadrokakhidze@gmail.com>
2026-07-30 03:17:04 +0400

    Fix fetch.ts

--- src/fetch.ts
+++ src/fetch.ts
@@ -1,12 +1,6 @@
 import type { Aircraft } from "./types/aircraft";
-import nmToKm from "./utils/converter";
 import aircraftData from "./data/aircraft.json";
 
 export async function fetchAircraftData(): Promise<Aircraft[]> {
-  const readableData = aircraftData.map((item: Aircraft) => {
-    item.specs.maxRangeKm = nmToKm(item.specs.maxRangeNm);
-    return item;
-  });
-
-  return readableData;
+  return aircraftData as Aircraft[];
 }
